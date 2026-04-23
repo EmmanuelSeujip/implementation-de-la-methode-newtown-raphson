aa337ae51f8cfd3577c630d73c9619d1d4757ed7
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-04-23 21:39:00 +0100

    Implémentation de la méthode de newton raphston

---
+++ methods/newton.h
@@ -0,0 +1,9 @@
+#include "../common/type.h"
+#include "../common/intervalle/intervalle.h"
+
+#ifndef NEWTON_H
+#define NEWTON_H
+
+Resultat newton(Polynome p, Intervalle I, double epsilon);
+
+#endif // NEWTON_H
